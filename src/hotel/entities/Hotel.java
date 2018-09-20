caab95d63242ba4295dcafb8f863f423840109c2
Dylan Kalpa <jpd.kalpa@gmail.com>
2018-09-20 19:40:01 +1000

    Implementation of the code block : public long book()

--- src/hotel/entities/Hotel.java
+++ src/hotel/entities/Hotel.java
@@ -6,15 +6,15 @@ import java.util.Map;
 
 import hotel.credit.CreditCard;
 import hotel.utils.IOUtils;
-
+//Dilan's Entity class
 public class Hotel {
-	
+
 	private Map<Integer, Guest> guests;
 	public Map<RoomType, Map<Integer,Room>> roomsByType;
 	public Map<Long, Booking> bookingsByConfirmationNumber;
 	public Map<Integer, Booking> activeBookingsByRoomId;
-	
-	
+
+
 	public Hotel() {
 		guests = new HashMap<>();
 		roomsByType = new HashMap<>();
@@ -26,7 +26,7 @@ public class Hotel {
 		activeBookingsByRoomId = new HashMap<>();
 	}
 
-	
+
 	public void addRoom(RoomType roomType, int id) {
 		IOUtils.trace("Hotel: addRoom");
 		for (Map<Integer, Room> rooms : roomsByType.values()) {
@@ -39,28 +39,28 @@ public class Hotel {
 		rooms.put(id, room);
 	}
 
-	
+
 	public boolean isRegistered(int phoneNumber) {
 		return guests.containsKey(phoneNumber);
 	}
 
-	
+
 	public Guest registerGuest(String name, String address, int phoneNumber) {
 		if (guests.containsKey(phoneNumber)) {
 			throw new RuntimeException("Phone number already registered");
 		}
 		Guest guest = new Guest(name, address, phoneNumber);
-		guests.put(phoneNumber, guest);		
+		guests.put(phoneNumber, guest);
 		return guest;
 	}
 
-	
+
 	public Guest findGuestByPhoneNumber(int phoneNumber) {
 		Guest guest = guests.get(phoneNumber);
 		return guest;
 	}
 
-	
+
 	public Booking findActiveBookingByRoomId(int roomId) {
 		Booking booking = activeBookingsByRoomId.get(roomId);;
 		return booking;
@@ -74,25 +74,29 @@ public class Hotel {
 			IOUtils.trace(String.format("Hotel: checking room: %d",room.getId()));
 			if (room.isAvailable(arrivalDate, stayLength)) {
 				return room;
-			}			
+			}
 		}
 		return null;
 	}
 
-	
+
 	public Booking findBookingByConfirmationNumber(long confirmationNumber) {
 		return bookingsByConfirmationNumber.get(confirmationNumber);
 	}
 
-	
-	public long book(Room room, Guest guest, 
+
+	public long book(Room room, Guest guest,
 			Date arrivalDate, int stayLength, int occupantNumber,
 			CreditCard creditCard) {
-		// TODO Auto-generated method stub
-		return 0L;		
+			Booking booking = room.book(guest, arrivalDate, stayLength, occupantNumber, creditCard);
+			long confirmationNumber = booking.getConfirmationNumber();
+			bookingsByConfirmationNumber.put(Long.valueOf(confirmationNumber), booking);
+
+
+			return confirmationNumber;
 	}
 
-	
+
 	public void checkin(long confirmationNumber) {
 		// TODO Auto-generated method stub
 	}
@@ -102,7 +106,7 @@ public class Hotel {
 		// TODO Auto-generated method stub
 	}
 
-	
+
 	public void checkout(int roomId) {
 		// TODO Auto-generated method stub
 	}
